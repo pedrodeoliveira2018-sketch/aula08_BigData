257b1f1b232dc3fde9a32ba41159e009b37d2c9a
PedroDeOliveira <PedroDeOliveira2018@icloud.com>
2025-12-09 21:56:38 -0300

    aula de def

---
+++ aula-08/exemplo02.py
@@ -0,0 +1,24 @@
+# def saudaçao(n):
+#     print(f'Olá {n} é a soma dos seus numeros  ')
+
+
+# na = int(input('digite o primeiro numero '))
+# ne = int(input('digite o segundo numero '))
+
+# total = na + ne
+
+# saudaçao(total)
+
+# nome = input(' informe seu nome : ')
+# idade = input ('informe sua idade : ')
+
+# saudaçao(nome , idade )
+
+def soma(n1,n2):
+    total = n1 + n2
+    print(f'Olá {total} é a soma dos seus numeros  ')
+
+
+na = int(input('digite o primeiro numero '))
+ne = int(input('digite o segundo numero '))
+soma(na,ne)
